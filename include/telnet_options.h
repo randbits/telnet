7e7ace7d4771959193bb4ab4041eef117f683122
randbits <hkgithub@gmail.com>
2013-03-28 23:09:56 +0800

    include/: add telnet option code definitions

---
+++ include/telnet_options.h
@@ -0,0 +1,15 @@
+/*
+ *	The Telnet Option Codes
+ *	
+ *	NOTE: 1. alll command start with TELNET_OPT_*
+ */
+
+#ifndef _TELNET_OPTIONS_H_
+#define	_TELNET_OPTIONS_H_
+
+/*rfc858.txt
+ *	TELNET SUPPRESS GO AHEAD OPTION
+ */
+#define TELNET_OPT_SUPPRESS_GO_AHEAD	3
+
+#endif /*_TELNET_OPTIONS_H_*/
