06bd586f15d4450495fb2bdd6e177da6673dd103
randbits <hkgithub@gmail.com>
2013-03-29 00:19:40 +0800

    include/telnet_options.h: add telnet_option structure and its name, descriptions accessors

--- include/telnet_options.h
+++ include/telnet_options.h
@@ -7,9 +7,25 @@
 #ifndef _TELNET_OPTIONS_H_
 #define	_TELNET_OPTIONS_H_
 
+typedef struct telnet_option telnet_option;
+struct telnet_option
+{
+    unsigned char  option_code; //option code. one byte.
+    const char*    option_name; //option name: e.g. "TELNET_OPT_"
+    const char*	   option_desc; //option description: e.g. "" 
+};
+
 /*rfc858.txt
  *	TELNET SUPPRESS GO AHEAD OPTION
  */
 #define TELNET_OPT_SUPPRESS_GO_AHEAD	3
+
+
+/*
+ *	Public APIs
+ */
+
+extern const char* telnet_option_get_name (unsigned char option_code);
+extern const char* telnet_option_get_desc (unsigned char option_code);
 
 #endif /*_TELNET_OPTIONS_H_*/
