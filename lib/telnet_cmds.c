bfe300dae06922bc921624b4a6fca219afe6f4ea
randbits <hkgithub@gmail.com>
2013-03-29 22:29:51 +0800

    lib/telnet_cmds.c: remove TELNET_CMD_* prefix in cmd_name

--- lib/telnet_cmds.c
+++ lib/telnet_cmds.c
@@ -10,89 +10,84 @@
 static telnet_cmd _telnet_cmds_table[] = {
     [0] = {//240
 	.cmd_code = TELNET_CMD_SE, 
-	.cmd_name = "TELNET_CMD_SE",
+	.cmd_name = "SE",
 	.cmd_desc = "Subnegotiation End",
     },
     [1] = {//241
 	.cmd_code = TELNET_CMD_NOP,
-	.cmd_name = "TELNET_CMD_NOP",
+	.cmd_name = "NOP",
 	.cmd_desc = "No Operation",
     },
     [2] = {//242
 	.cmd_code = TELNET_CMD_DM,
-	.cmd_name = "TELNET_CMD_DM",
+	.cmd_name = "DM",
 	.cmd_desc = "Data Mark",
     },
     [3] = {//243
 	.cmd_code = TELNET_CMD_BRK,
-	.cmd_name = "TELNET_CMD_BRK",
+	.cmd_name = "BRK",
 	.cmd_desc = "NVT Break",
     },
     [4] = {//244
 	.cmd_code = TELNET_CMD_IP,
-	.cmd_name = "TELNET_CMD_IP",
+	.cmd_name = "IP",
 	.cmd_desc = "Interrupt Process",
     },
     [5] = {//245
 	.cmd_code = TELNET_CMD_AO,
-	.cmd_name = "TELNET_CMD_AO",
+	.cmd_name = "AO",
 	.cmd_desc = "Abort Output",
     },
     [6] = {//246
 	.cmd_code = TELNET_CMD_AYT,
-	.cmd_name = "TELNET_CMD_AYT",
+	.cmd_name = "AYT",
 	.cmd_desc = "Are You There",
     },
     [7] = {//247
 	.cmd_code = TELNET_CMD_EC,
-	.cmd_name = "TELNET_CMD_EC",
+	.cmd_name = "EC",
 	.cmd_desc = "Erase Character",
     },
     [8] = {//248
 	.cmd_code = TELNET_CMD_EL,
-	.cmd_name = "TELNET_CMD_EL",
+	.cmd_name = "EL",
 	.cmd_desc = "Erase Line",
     },
     [9] = {//249
 	.cmd_code = TELNET_CMD_GA,
-	.cmd_name = "TELNET_CMD_GA",
+	.cmd_name = "GA",
 	.cmd_desc = "Go Ahead",
     },
     [10] = {//250
 	.cmd_code = TELNET_CMD_SB,
-	.cmd_name = "TELNET_CMD_SB",
+	.cmd_name = "SB",
 	.cmd_desc = "Subnegotiation Begin",
     },
     [11] = {//251
 	.cmd_code = TELNET_CMD_WILL,
-	.cmd_name = "TELNET_CMD_WILL",
+	.cmd_name = "WILL",
 	.cmd_desc = "Will",
     },
     [12] = {//252
 	.cmd_code = TELNET_CMD_WONT,
-	.cmd_name = "TELNET_CMD_WONT",
+	.cmd_name = "WONT",
 	.cmd_desc = "Will not",
     },
     [13] = {//253
 	.cmd_code = TELNET_CMD_DO,
-	.cmd_name = "TELNET_CMD_DO",
+	.cmd_name = "DO",
 	.cmd_desc = "Do",
     },
     [14] = {//254
 	.cmd_code = TELNET_CMD_DONT,
-	.cmd_name = "TELNET_CMD_DONT",
+	.cmd_name = "DONT",
 	.cmd_desc = "Do not",
     },
     [15] = {//255
 	.cmd_code = TELNET_IAC,
-	.cmd_name = "TELNET_IAC",
+	.cmd_name = "IAC",
 	.cmd_desc = "Interprete As Command",
     }
-//   {//Invalid
-//	.cmd_code = TELNET_INVALID,
-//	.cmd_name = "TELNET_INVALID",
-//	.cmd_desc = "Invalid Telnet Command",
-//   }
 };
 
 TELNET_EXPORT
